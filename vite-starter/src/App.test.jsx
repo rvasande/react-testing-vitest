8129c4c3ac2e9e7d9784195a951203c1de9270c7
rushikesh <rushikeshvasande333@gmail.com>
2024-11-18 23:22:27 +0530

    first test case

--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -1,8 +1,10 @@
 import { render, screen } from "@testing-library/react";
+import { logRoles } from "@testing-library/react";
 import App from "./App";
 
-test("App contains correct heading", () => {
-  render(<App />);
-  const headingElement = screen.getByText(/learn react/i);
-  expect(headingElement).toBeInTheDocument();
+test("button start with correct color ", () => {
+const {container} = render(<App />)
+// logRoles(container)
+const buttonElement = screen.getByRole('button',{name:/blue/i})
+expect(buttonElement).toHaveClass('red')
 });
